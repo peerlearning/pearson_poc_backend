b92701a975afbe35d88ed67fe084d298d5feb828
Rupesh Joshi <rupesh.joshi98@gmail.com>
2019-11-05 13:56:47 +0530

    calculation fixes

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,7 +2,6 @@ class TestController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def next_problem
-
     #right_ans_count => R
     #problem_number => L
     #Difficulty used  => H
@@ -13,7 +12,7 @@ class TestController < ApplicationController
     problem_id = params['problem_id']
     problem_number = params['problem_number'].to_i
     difficulty_used = params['difficulty_used'].to_i
-    right_ans_count = params['right_ans_count'].to_i
+    right_ans_count = params['right_ans_count'].to_f
 
     test = Test.where(id: test_id).first
 
@@ -21,32 +20,33 @@ class TestController < ApplicationController
 
     if current_problem['answer'].first == student_answer
       right_ans_count += 1
-      approx_difficulty =  current_problem['difficulty_rating'].to_i + 2/problem_number.to_i
+      next_approx_difficulty =  current_problem['item_difficulty'].to_i + 2/problem_number.to_i
     else
-      approx_difficulty =  current_problem['difficulty_rating'].to_i - 2/problem_number.to_i
+      next_approx_difficulty =  current_problem['item_difficulty'].to_i - 2/problem_number.to_i
     end
 
     wrong_answers = problem_number.to_i - right_ans_count
+
     if wrong_answers == 0
 
       ability_estimate =  difficulty_used/problem_number.to_f + Math.log((right_ans_count-0.5)/ (wrong_answers + 0.5), 2.71)
-      estimate_error = (problem_number.to_f/(right_ans_count - 0.5 * wrong_answers + 0.5)) **2
+      estimate_error = (problem_number.to_f/(right_ans_count - 0.5 * wrong_answers + 0.5)) **1/2
 
     elsif right_ans_count == 0
       ability_estimate =  difficulty_used/problem_number.to_f + Math.log((right_ans_count+0.5)/ (wrong_answers - 0.5), 2.71)
-      estimate_error = (problem_number.to_f/(right_ans_count + 0.5 * wrong_answers - 0.5)) **2
+      estimate_error = (problem_number.to_f/(right_ans_count + 0.5 * wrong_answers - 0.5)) **1/2
     else
       ability_estimate =  difficulty_used/problem_number.to_f + Math.log((right_ans_count / wrong_answers), 2.71)
-      estimate_error = (problem_number.to_f/(right_ans_count * wrong_answers)) **2
+      estimate_error = (problem_number.to_f/(right_ans_count * wrong_answers)) **1/2
     end
 
 
-    if approx_difficulty >= 2 
-      prob_difficulty = 3
-    elsif approx_difficulty >= 0 && approx_difficulty < 2
+    if next_approx_difficulty > 1
       prob_difficulty = 2
-    elsif approx_difficulty < 0
-      prob_difficulty = 1
+    elsif next_approx_difficulty >= -1 && next_approx_difficulty <= 1
+      prob_difficulty = 0
+    elsif next_approx_difficulty < -1
+      prob_difficulty = -2
     else
       #do nothing
     end
@@ -56,7 +56,7 @@ class TestController < ApplicationController
     problems = @str.problems 
     problems.push({problem_id: problem_id, answer: student_answer,
                       cms_answer: current_problem['answer'], 
-                      problem_difficulty: current_problem['difficulty_rating'],
+                      problem_difficulty: current_problem['item_difficulty'],
                       ability_estimate: ability_estimate,
                       estimate_error: estimate_error
                       })
@@ -67,25 +67,21 @@ class TestController < ApplicationController
     if next_problem.blank? || problem_number >= 15
       redirect_to(test_summary_path(test_id: test.id, student_id: params[:student_id]))
     else
-        # if next_problem
       next_problem['number'] = problem_number.to_i+1
       next_problem['difficulty_used'] = difficulty_used.to_i + prob_difficulty.to_i
       next_problem['right_ans_count'] = right_ans_count
       render json: next_problem, status: :ok
-    # else
-    #   redirect_to(test_summary_path(test_id: test.id, student_id: params[:student_id]))
     end
   end
 
   def get_next_problem(problems, student_answer, prob_difficulty)
-    problem_ids = @str.problems.pluck(:problem_id)
-    selected_problems = problems.select{|prob| prob if (prob[:difficulty_rating] == prob_difficulty.to_s)}.select{|pblm| pblm  unless problem_ids.include? pblm[:id] }
 
+    problem_ids = @str.problems.pluck(:problem_id)
+    selected_problems = problems.select{|prob| prob if (prob[:item_difficulty].to_i == prob_difficulty.to_i)}.select{|pblm| pblm  unless problem_ids.include? pblm[:id] }
     selected_problems.sample
   end
 
   def start
-
     #test = Test.find_by(params[:id])
     @test = Test.first
     str = StudentTestRecord.create(student_id: params[:student_id], test_id: @test.to_param)
@@ -95,7 +91,6 @@ class TestController < ApplicationController
     @difficulty_used = 0
     @right_ans_count = 0
     # render json: @test, status: :ok
-
     # problem = test.problem.first
     # problem_str
   end
